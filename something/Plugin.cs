bdb0d95b570faf6d99e368f5d64343c19ce9f822
o0F-0oF <76185649+o0F-0oF@users.noreply.github.com>
2022-03-20 13:27:37 -0400

    files uploaded

---
+++ something/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx.Configuration;
+using BepInEx.IL2CPP;
+using BepInEx.Logging;
+using BepInEx;
+
+namespace something
+{
+    [BepInPlugin("amongus.sus.red.plugins", "something", "1.1.1.1")]
+    public class Plugin : BasePlugin
+    {
+        internal static ManualLogSource log;
+        internal static ConfigFile cfg;
+        public override void Load()
+        {
+            log = Log;
+            cfg = Config;
+            log.LogMessage("BepInEx is very cool");
+        }
+    }
+}
